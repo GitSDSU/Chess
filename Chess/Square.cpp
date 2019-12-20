b3d7faf6033a9681048417175cbfb48235d29515
GitSDSU <lurzua.sdsu@gmail.com>
2019-12-20 12:11:34 -0800

    Display Square

--- Chess/Square.cpp
+++ Chess/Square.cpp
@@ -18,39 +18,10 @@ Square::~Square()
 
 void Square::Display_Square()
 {
-	std::string col;
-	switch (position.col)
-	{
-	case _A:
-		col = "A";
-		break;
-	case _B:
-		col = "B";
-		break;
-	case _C:
-		col = "C";
-		break;
-	case _D:
-		col = "D";
-		break;
-	case _E:
-		col = "E";
-		break;
-	case _F:
-		col = "F";
-		break;
-	case _G:
-		col = "G";
-		break;
-	case _H:
-		col = "H";
-		break;
-	default:
-		break;
-	}
-	std::cout << col <<
+	std::string col[] = { "A", "B", "C", "D", "E", "F", "G", "H" };
+	std::cout << col[position.col-1] <<
 		std::to_string(position.row);
-	if (col[0] == 'H')
+	if (col[position.col - 1] == "H")
 	{
 		std::cout << std::endl;
 	}
